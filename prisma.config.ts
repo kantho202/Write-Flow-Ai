26af0900d6d889bed4d24defc0d5684d3db8b1b4
kantho202 <kanthochakma2025@gmail.com>
2026-06-03 01:01:50 +0600

    project deploy to vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
   migrations: {
+    database: {
+      url: process.env.DATABASE_URL!,
+    },
     seed: "npx tsx prisma/seed.ts",
   },
 });
